2907d9402e75fca53ca2c9fd28dfc40d8560690e
Goh Yong Siang <gysiang@gmail.com>
2022-02-13 01:24:22 +0800

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello world');
+import express from 'express';
+import { read } from './jsonFileStorage.js';
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+const handleIncomingRequest = (request, response) => {
+  console.log('request came in');
+
+  read('data.json', (err, data) => {
+    const { sightings } = data;
+    console.log(sightings);
+    response.render('home', { sightings });
+  });
+};
+
+app.get('/sighting', handleIncomingRequest);
+app.listen(3004);
