7772a41ec2a6b7f8bd91029e8a1d1ad0aad34ab3
Noel Lim <noellimx@gmail.com>
2022-02-14 03:22:09 +0800

    add 501 routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,52 @@
+import e from "express";
+
+/**
+ *
+ * @param {e.Express} app
+ */
 const attachRoutes = (app) => {
+  app.get("/sighting", (req, res) => {
+    console.log("Route GET /sighting");
+    res.sendStatus(501);
+  });
+
+  app.post("/sighting", (req, res) => {
+    console.log("Route POST /sighting");
+    res.sendStatus(501);
+  });
+
+  app.get("/sighting:index", (req, res) => {
+    console.log("Route POST /sighting");
+    res.sendStatus(501);
+  });
+
   app.get("/", (req, res) => {
     console.log("Route GET /");
-    res.render("home");
+    res.sendStatus(501);
+  });
+
+  app.get("/sighting/:index/edit", (req, res) => {
+    console.log("Route GET /");
+    res.sendStatus(501);
+  });
+
+  app.put("/sighting/:index/edit", (req, res) => {
+    console.log("Route PUT /");
+    res.sendStatus(501);
+  });
+  app.delete("/sighting/:index/edit", (req, res) => {
+    console.log("Route DELETE /");
+    res.sendStatus(501);
+  });
+
+  app.get("/shapes", (req, res) => {
+    console.log("Route GET /");
+    res.sendStatus(501);
+  });
+
+  app.get("/shapes/:shape", (req, res) => {
+    console.log("Route GET /");
+    res.sendStatus(501);
   });
 };
 
