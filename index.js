09c671de15e48bfb331b5dbbe92299c239368674
Goh Yong Siang <gysiang@gmail.com>
2022-02-15 00:36:49 +0800

    added post route for sightings page

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
 import express from 'express';
-import { read } from './jsonFileStorage.js';
+import { read, add } from './jsonFileStorage.js';
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+// app.use(express.urlencoded({ extended: false }));
 
-const handleIncomingRequest = (request, response) => {
+const handleIncomingForm = (request, response) => {
+  response.render('home');
+};
+
+const handleIndividualSighting = (request, response) => {
   console.log('request came in');
 
+  const sightingIndex = request.params.index;
+
   read('data.json', (err, data) => {
-    const { sightings } = data;
-    console.log(sightings);
-    response.render('home', { sightings });
+    if (err) {
+      console.log('read error!');
+    }
+    const sighting = data.sightings[sightingIndex];
+
+    response.render('singleSight', { sightings: sighting, index: sightingIndex });
   });
 };
 
-app.get('/sighting', handleIncomingRequest);
+const handlePostIncomingForm = (request, response) => {
+  console.log('request came in');
+
+  add('data.json', 'sightings', request.body, (err, data) => {
+    if (err) {
+      console.log('read error');
+      return ('read error');
+    }
+
+    read('data.json', (err, data) => {
+      if (err) {
+        console.log('read error');
+        return ('read error');
+      }
+
+      const index = data.sightings.length - 1;
+
+      response.redirect(`/sighting/${index}`);
+    });
+  });
+};
+
+app.get('/sighting', handleIncomingForm);
+app.get('/sighting/:index', handleIndividualSighting);
+app.post('/sighting', handlePostIncomingForm);
+
 app.listen(3004);
