1b7344f578cf8be7f3e260a03acdaaa2deab4e8c
Noel Lim <noellimx@gmail.com>
2022-02-16 03:25:09 +0800

    refactor edit

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 import e from "express";
-import { addSighting } from "../dataOperations/index.js";
-import { read, readFile, writeFile } from "fs";
+import { readFile, writeFile } from "fs";
 
 const dataPath = "data.json";
 
@@ -12,8 +11,12 @@ const writePath = dataPath;
  * @param {e.Express} app
  */
 const attachRoutes = (app) => {
-  const newSightingConsumerRoute = "/sighting";
-  const routePathViewSightingOne = `${newSightingConsumerRoute}/:index`;
+  const routeNewSightingForm = "/sighting-form";
+  const routeSightingConsumerNew = "/sighting-add";
+
+  const routeEditSightingSuffix = `/sighting-form-edit`;
+  const routeEditSightingForm = `${routeEditSightingSuffix}/:index`;
+  const routeSightingConsumerEdit = "/sighting-edit";
 
   const renderViewSightingOne = (req, res) => {
     const { params } = req;
@@ -23,20 +26,43 @@ const attachRoutes = (app) => {
         throw err;
       }
       const json = JSON.parse(content);
-      res.render("sighting", { isNew, index, sighting: json.sightings[index] });
+      res.render("sighting", {
+        isNew,
+        index,
+        sighting: json.sightings[index],
+        editRoute: `${routeEditSightingSuffix}/${index}`,
+      });
       return;
     });
   };
-  // input form
-  app.get("/sighting-form", (req, res) => {
-    console.log("Route GET /sighting");
-    return res.render("sighting-form", { postRoute: newSightingConsumerRoute });
+
+  const renderEditSightingOne = (req, res) => {
+    const { params } = req;
+    const { index } = params;
+
+    readFile(readPath, (err, content) => {
+      if (err) {
+        throw err;
+      }
+      const json = JSON.parse(content);
+      return res.render("sighting-edit", {
+        index,
+        sighting: json.sightings[index],
+        postRoute: routeSightingConsumerEdit,
+      });
+    });
+  };
+  // input form - new
+  app.get(routeNewSightingForm, (req, res) => {
+    return res.render("sighting-form", { postRoute: routeSightingConsumerNew });
   });
+  // input form - edit
+  app.get(routeEditSightingForm, renderEditSightingOne);
 
   app.post(
-    newSightingConsumerRoute,
+    routeSightingConsumerNew,
     (req, res, next) => {
-      console.log(`Route POST ${newSightingConsumerRoute}`);
+      console.log(`Route POST ${routeSightingConsumerNew}`);
 
       const { body } = req;
       const { text, date_time, city, state, duration, summary } = body;
@@ -62,13 +88,54 @@ const attachRoutes = (app) => {
     renderViewSightingOne
   );
 
-  app.get(
-    routePathViewSightingOne,
-    (req, res) => {
-      console.log(`GET ${newSightingConsumerRoute}/:index`);
-    },
-    renderViewSightingOne
-  );
+  app.post(routeSightingConsumerEdit, (req, res, next) => {
+    console.log(`Route POST ${routeSightingConsumerEdit}`);
+
+    const { body } = req;
+    const {
+      text: newText,
+      date_time: newDate_time,
+      city: newCity,
+      state: newState,
+      duration: newDuration,
+      summary: newSummary,
+      index: targetIndex,
+    } = body;
+
+    console.log("body");
+    console.log(body);
+
+    readFile(readPath, (err, content) => {
+      if (err) {
+        throw err;
+      }
+      const json = JSON.parse(content);
+      const { sightings } = json;
+      const targetSighting = sightings[targetIndex];
+      const { text, date_time, city, state, duration, summary } =
+        targetSighting;
+      const editedSighting = {
+        ...targetSighting,
+        text: newText ? newText : text,
+        date_time: newDate_time ? newDate_time : date_time,
+        city: newCity ? newCity : city,
+        state: newState ? newState : state,
+        duration: newDuration ? newDuration : duration,
+        summary: newSummary ? newSummary : summary,
+      };
+      const newSightings = [...sightings];
+      newSightings[targetIndex] = editedSighting;
+      const newJson = { ...json, sightings: newSightings };
+      const newContent = JSON.stringify(newJson);
+      writeFile(writePath, newContent, (err) => {
+        if (err) {
+          throw err;
+        }
+        req.params.index = targetIndex;
+        return renderViewSightingOne(req, res);
+      });
+    });
+  });
 
   app.get("/", (req, res) => {
     console.log("Route GET /");
