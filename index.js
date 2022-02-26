cbaa80f7c95c5aa14b53f466ae0634df35b6de0d
Nyx92 <ngyx92@gmail.com>
2022-02-27 01:03:24 +0800

    favorites-feature-added

--- index.js
+++ index.js
@@ -74,7 +74,6 @@ app.get('/sighting/:index', (request, response) => {
     // stores the specific object in the array into dataIndex object with key dataIndex
     const dataIndex = { sightings: [data.sightings[request.params.index]] };
     dataIndex.visits = [data.visits[0]];
-    console.log(dataIndex);
     response.render('sightings-index', dataIndex);
   });
 });
@@ -82,14 +81,83 @@ app.get('/sighting/:index', (request, response) => {
 /**
   * A function to store favorites
  */
-// app.get('/fav/:index', (request, response) => {
-//   // check if it's not the first time a request has been made
-//   // if there is already a request, then not unique.
-//   if (request.cookies.visits) {
-//     visits = Number(request.cookies.visits); // get the value from the request
+
+// // push favorite into an array
+//   read('data.json', (err, data) => {
+//     if (err) {
+//       response.status(500).send('DB read error.');
+//       return;
+//     }
+//     arrayOfFavorites.push(data.sightings[favourite])
 //   }
 
-// }
+app.get('/fav', (request, response) => {
+  // favouriteIndex now takes the index value of the favorited item
+  const favouriteIndex = request.query.index;
+  const arrayOfFavorites = [];
+  // if 'favorite' cookie exists, then try and add current favorite index into arrayOfFavorites
+  if (request.cookies.favorite) {
+    console.log('this run2');
+    // take original VALUES from favorite KEY in the cookies and parse it so we can work on it
+    let originalArray = JSON.parse(request.cookies.favorite);
+    console.log(originalArray);
+    // next, take new value of favorited item i.e., favouriteIndex and try and add into array
+    // will need it to NOT add if it already exist
+    const mySet = new Set(originalArray);
+    mySet.add(favouriteIndex);
+    originalArray = Array.from(mySet);
+    // Original Array will be the full set of indexes of our favorite items
+    console.log(originalArray);
+    const newArrayOfFavoritesString = JSON.stringify(originalArray);
+    console.log(newArrayOfFavoritesString);
+    response.cookie('favorite', newArrayOfFavoritesString);
+    // I will now need to find a way to pick out those data
+    read('data.json', (err, data) => {
+      const storedFavItem = [];
+      if (err) {
+        response.status(500).send('DB read error.');
+      }
+      for (let v = 0; v < originalArray.length; v++) {
+        for (let i = 0; i < data.sightings.length; i++) {
+          if (Number(originalArray[v]) === data.sightings[i].index) {
+          // store data
+            storedFavItem.push(data.sightings[i]);
+          }
+        }
+        console.log(storedFavItem);
+      }
+      const test = { sightings: storedFavItem };
+      console.log(test);
+      response.render('fav', test);
+    });
+  } else {
+    // if 'favorite' does not exist then create it
+    // first add into arrayofFavorites
+    arrayOfFavorites.push(favouriteIndex);
+    console.log(arrayOfFavorites);
+    const arrayOfFavoritesString = JSON.stringify(arrayOfFavorites);
+    console.log(arrayOfFavoritesString);
+    response.cookie('favorite', arrayOfFavoritesString);
+    // I will now need to find a way to pick out those data
+    read('data.json', (err, data) => {
+      const storedFavItem = [];
+      if (err) {
+        response.status(500).send('DB read error.');
+      }
+      for (let v = 0; v < arrayOfFavorites.length; v++) {
+        for (let i = 0; i < data.sightings.length; i++) {
+          if (Number(arrayOfFavorites[v]) === data.sightings[i].index) {
+          // store data
+            console.log('this run1?');
+            storedFavItem.push(data.sightings[i]);
+          }
+        }
+      }
+      const test = { sightings: storedFavItem };
+      response.render('fav', test);
+    });
+  }
+});
 
 /**
   * A function to render a list of sightings, also the HOME page
@@ -100,12 +168,12 @@ app.get('/', (request, response) => {
   let uniqueVisitorTest = 0;
   // check if it's not the first time a request has been made
   // if there is already a request, then not unique.
+  // Note that visits in request.cookies.visits refers to the KEY of the response.cookie(KEY, VALUE)
   if (request.cookies.visits) {
     visits = Number(request.cookies.visits); // get the value from the request
   }
   // if it's the first time
   if (request.cookies.visits === undefined) {
-    console.log('cookies.visits is undefined');
     uniqueVisitorTest += 1;
     // Will need to write the current Visit Count to 0
     read('data.json', (err, data) => {
@@ -113,9 +181,7 @@ app.get('/', (request, response) => {
         response.status(500).send('DB read error.');
         return;
       }
-      console.log(data.visits[0].CURRENTVISITCOUNT);
       data.visits[0].CURRENTVISITCOUNT = 0;
-      console.log(data.visits[0].CURRENTVISITCOUNT);
       write('data.json', data, (err) => {
         console.log('This should only write if I had cleared cookie before hand; must also write first, when this writes, currentVisitCount should go back to 1');
       });
